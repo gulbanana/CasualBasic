486e9f108a92467d09883055f3e6a82b18569dd2
Thomas Castiglione <thomas@agcom.com.au>
2014-06-27 14:14:24 +0800

    limit effect to VB files

---
+++ CreationListener.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Casual_Basic
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("Basic")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class CreationListener : IWpfTextViewCreationListener
+    {
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name("Casual_Basic")]
+        [Order(After = PredefinedAdornmentLayers.Selection, Before = PredefinedAdornmentLayers.Text)]
+        public AdornmentLayerDefinition editorAdornmentLayer = null;
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            new Casual_Basic(textView);
+        }
+    }
+}
